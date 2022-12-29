95558b0ad074f0066d2899ae6ebef68f33078f95
Will Hawkins <whh8b@obs.cr>
2022-12-29 16:14:47 -0500

    Actually add the files.

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class A {
+public:
+  void alpha(std::string s) { std::cout << "Alpha (of string) (in A)\n"; }
+  void alpha(std::vector<std::string> vec) {
+    std::cout << "Alpha (of vector) (in A)\n";
+  }
+  void beta(std::string s) { std::cout << "Beta (of string) (in A)\n"; }
+  void beta(std::vector<std::string> vec) {
+    std::cout << "Beta (of vector) (in A)\n";
+  }
+};
+
+class AA : public A {
+public:
+};
+
+class AAA : public AA {
+public:
+  void beta(std::string s) { std::cout << "Beta (of string) in AAA\n"; }
+  void beta(std::vector<std::string> vec) {
+    std::cout << "Beta (of vector) in AAA\n";
+  }
+  void alpha(std::string s) { std::cout << "Alpha (of string) in AAA\n"; }
+};
+
+void overload_resolution_example() {
+  std::string s{};
+  std::vector<std::string> v{};
+
+  A a;
+  AA aa;
+  AAA aaa;
+
+  aaa.alpha(s); // S(alpha, AAA) = { { alpha(std::string) }, { AAA } }
+  // aaa.alpha(v); // error: S(alpha, AAA) = { { alpha(std::string) }, { AAA } }
+  // only. The search stops there.
+
+  aaa.beta(s); // S(beta, AAA) = { { beta(std::string),
+               // beta(std::vector<std::string>) }, { AAA } } (those are
+               // immediately available and no subclasses are considered.)
+  aaa.beta(v); // S(beta, AAA) = { { beta(std::string),
+               // beta(std::vector<std::string>) }, { AAA } } (those are
+               // immediately available and no subclasses are considered.)
+
+  aa.alpha(s); // S(alpha, AA) = S(alpha, A) (because S(alpha, AA)
+               // is empty, search continues in base classes!)
+               // ie, S(alpha, AA) = { { alpha(std::string),
+               // alpha(std::vector<std::string> }, { A } }.
+  aa.alpha(v); // S(alpha, AA) = S(alpha, A) (because S(alpha, AA)
+               // is empty, search continues in base classes!)
+               // ie, S(alpha, AA) = { { alpha(std::string),
+               // alpha(std::vector<std::string> }, { A } }.
+}
+
+template <class T> class Base {
+public:
+  void setter(const T &) { std::cout << "Setter in Base\n"; }
+  void getter() { std::cout << "Getter in Base\n"; }
+};
+
+template <class T> class Derived : public Base<T> {
+public:
+  virtual void setter(const T &) { std::cout << "Setter in Derived\n"; }
+  virtual void getter() { std::cout << "Getter in Derived\n"; }
+};
+
+template <class T> class BaseV {
+public:
+  virtual void setter(const T &) { std::cout << "Setter in BaseV\n"; }
+  virtual void getter() { std::cout << "Getter in BaseV\n"; }
+};
+
+template <class T> class DerivedV : public BaseV<T> {
+public:
+  virtual void setter(const T &) { std::cout << "Setter in DerivedV\n"; }
+  virtual void getter() { std::cout << "Getter in DerivedV\n"; }
+};
+
+void virtual_function_example() {
+
+  Derived<int> d{};
+  Base<int> b{d};
+  DerivedV<int> dv{};
+  BaseV<int> &bv{dv};
+  // BaseV<int> vv{dv}; // Does not work as expected: No matter that we have
+  // declared the functions to be virtual,
+  // the fact that vv is *not* a reference nor a pointer, slicing occurs and
+  // the static type of the object is used for dispatching.
+
+  b.setter(5);
+  b.getter();
+
+  bv.setter(5);
+  bv.getter();
+}
+
+int main() {
+  overload_resolution_example();
+  virtual_function_example();
+  return 0;
+}
